56e02360011fbe692038df16d581f33dd5ea42b1
Rtlich <raniatlich1@gmail.com>
2023-10-28 05:34:26 +0100

    symfony

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -29,24 +29,77 @@ class BookRepository extends ServiceEntityRepository
         ->getQuery()
         ->getResult();
     }
-    public function trie($username = null)
+    public function trie()
+    {
+        $qb = $this->createQueryBuilder('b')
+            ->join('b.author', 'a')
+            ->orderBy('a.username', 'ASC');
+
+        return $qb->getQuery()->getResult();
+    }
+    public function livrepub()
+
+    {return $this->createQueryBuilder('book')
+    ->join('book.author', 'author')
+    ->Where('author.nb_books > 35')
+    ->andWhere('book.publicationDate < :date')
+    ->groupBy('author.nb_books')
+    ->setParameter('date', new \DateTime('2023-01-01'))
+    ->getQuery()
+    ->getResult();
+    }
+public function editw()
+   { return $this->createQueryBuilder('book')
+            ->innerJoin('book.author', 'author')
+            ->where('author.username = :authorName')
+            ->setParameter('authorName', 'wiliam Shakespear')
+            ->getQuery()
+            ->getResult();
+   }
+   public function sumCategory()
+   {
+       $entityManager = $this->getEntityManager();
+       $query=$entityManager->createQuery(
+
+         "SELECT COUNT(b.ref) as total
+               FROM  App\Entity\Book b
+               WHERE b.category = :category");
+
     
-        {
-            $qb = $this->createQueryBuilder('u');
-            $qb->andWhere('u.username LIKE :username');
-                
-            if ($username) {
-                $qb->andWhere("u.username LIKE :username")
-                    ->setParameter('username', $username);
-            }
-            
-            // Add an orderBy clause for the 'username' field in ascending order.
-            $qb->orderBy('u.username', 'ASC');
-            
-            return $qb->getQuery();
-        }
+       $query->setParameter('category', 'Science-Fiction');
+
+       return $query->getSingleScalarResult();
+   }
+   public function afficherliste ()
+
+  { $a = $this->getEntityManager();
+        $query = $a->createQuery(
+            'SELECT book
+             FROM App\Entity\Book book
+             WHERE book.publicationDate between :startDate and :endDate'
+        );
+        $query->setParameter('startDate', '2014-01-01');
+        $query->setParameter('endDate', '2018-12-31');
+        return $query->getResult();
     }
 
+    public function minmax($minNumber,$maxNumber)
+    {
+        {
+            $A = $this->getEntityManager();
+            $query = $A->createQuery(
+                'SELECT book
+                FROM App\Entity\Book book
+                JOIN book.author author
+                where author.nb_books between :min  and :max
+                GROUP BY author.nb_books
+            ');
+    
+    $query->setParameter('minNumber', $minNumber);
+    $query ->setParameter('maxNumber', $maxNumber);
+              return  $query->getResult();
+        }
+    }
 
 //    /**
 //     * @return Book[] Returns an array of Book objects
@@ -72,4 +125,4 @@ class BookRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
-
+}
